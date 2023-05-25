19c8d54313f97c659078717de304d3f8f1e19a63
k2c-cacc <129217040+k2c-cacc@users.noreply.github.com>
2023-05-25 13:49:53 -0300

    python ex075

--- pythonExercicio/ex075.py
+++ pythonExercicio/ex075.py
@@ -1,1 +1,26 @@
-tupla = ()
+tupla = (int(input("Digite o 1o número: ")),
+         int(input("Digite o 2o número: ")),
+         int(input("Digite o 3o número: ")),
+         int(input("Digite o 4o número: ")))
+print(tupla)
+#print(tupla.count(9))
+print(f'O número 9 apareceu {tupla.count(9)} vez')
+if 3 in tupla:
+   print(f'Número 3 apareceu na posição: {tupla.index(3) + 1}a')
+else:
+    print(0)
+pares = ''
+for c in tupla:
+    if c % 2 == 0:
+        pares += str(c) + ' '
+print(f'Apareceram esses números pares: {pares}')
+pares = ''
+
+for c in range(0, len(tupla)):
+    if tupla[c] % 2 == 0:
+        pares += str(tupla[c]) + ' '
+print(f'Apareceram esses números pares: {pares}')
+
+
+
+
